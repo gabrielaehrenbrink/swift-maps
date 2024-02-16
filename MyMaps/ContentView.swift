aa53982de9cbf0a310680680613ca4da0fb8da4a
Gabriela <gabrielaehrenbrink@gmail.com>
2024-02-16 14:25:31 +0000

    Initial Commit

---
+++ MyMaps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMaps
+//
+//  Created by Gabriela Ehrenbrink on 16/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyMaps/MyMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapsApp.swift
+//  MyMaps
+//
+//  Created by Gabriela Ehrenbrink on 16/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
