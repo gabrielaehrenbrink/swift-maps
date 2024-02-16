7e9ab1fb13f4b2ad96de58db9f8523927c20a2ba
Gabriela <gabrielaehrenbrink@gmail.com>
2024-02-16 18:25:18 +0000

    readme added

--- MyMaps/ContentView.swift
+++ MyMaps/ContentView.swift
@@ -4,20 +4,137 @@
 //
 //  Created by Gabriela Ehrenbrink on 16/02/2024.
 //
-
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cameraPosition: MapCameraPosition = .region(.userRegion)
+    @State private var searchText = ""
+    @State private var results = [MKMapItem]()
+    @State private var mapSelection: MKMapItem?
+    @State private var showDetails = false
+    @State private var getDirections = false
+    @State private var routeDisplaying = false
+    @State private var route: MKRoute?
+    @State private var routeDestination: MKMapItem?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Map(position: $cameraPosition, selection: $mapSelection) {
+            Annotation("My location", coordinate: .userLocation) {
+                ZStack {
+                    Circle()
+                        .frame(width:32, height: 32)
+                        .foregroundColor(.pink.opacity(0.25))
+                    Circle()
+                        .frame(width:20, height: 20)
+                        .foregroundColor(.white)
+                    Circle()
+                        .frame(width:12, height: 12)
+                        .foregroundColor(.pink)
+                }
+            }
+            
+            ForEach(results, id: \.self) { item in
+                if routeDisplaying {
+                    if item == routeDestination {
+                        let placemark = item.placemark
+                        Marker(placemark.name ?? "", coordinate: placemark.coordinate)
+                    }
+                } else {
+                    let placemark = item.placemark
+                    Marker(placemark.name ?? "", coordinate: placemark.coordinate)
+                }
+                }
+            if let route {
+                MapPolyline(route.polyline)
+                    .stroke(.pink, lineWidth: 6)
+            }
+            
         }
-        .padding()
+        
+        .overlay(alignment: .top) {
+            TextField("Seach for a location...", text: $searchText)
+                .font(.subheadline)
+                .padding(12)
+                .background(.white)
+                .padding()
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+        }
+        .onSubmit(of: .text) {
+            Task { await searchPlaces() }
+        }
+        .onChange(of: getDirections, { oldValue, newValue in
+            if newValue {
+                fetchRoute()
+            }})
+        .onChange(of: mapSelection, { oldValue, newValue in
+            showDetails = newValue != nil
+        })
+        .sheet(isPresented: $showDetails, content: {
+            LocationDetailsView(mapSelection: $mapSelection, show: $showDetails, getDirections: $getDirections)
+                .presentationDetents([.height(340)])
+                .presentationBackgroundInteraction(.enabled(upThrough: .height(340)))
+                .presentationCornerRadius(12)
+        } )
+        .mapControls {
+            MapCompass()
+            MapUserLocationButton()
+        }
     }
 }
+
+extension ContentView {
+    func searchPlaces() async {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchText
+        request.region = .userRegion
+        
+        let results = try? await MKLocalSearch(request: request).start()
+        self.results = results?.mapItems ?? []
+    }
+    func fetchRoute() {
+        if let mapSelection {
+            let request = MKDirections.Request()
+            request.source = MKMapItem(placemark: .init(coordinate: .userLocation))
+            request.destination = mapSelection
+            
+            Task {
+                let result = try? await MKDirections(request: request).calculate()
+                route = result?.routes.first
+                routeDestination = mapSelection
+                
+                withAnimation(.snappy) {
+                    routeDisplaying = true
+                    showDetails = false
+                    
+                    if let rect = route?.polyline.boundingMapRect, routeDisplaying {
+                        cameraPosition = .rect(rect)
+                    }
+                }
+            }
+        }
+    }
+    
+}
+
+
+extension CLLocationCoordinate2D {
+    static var userLocation: CLLocationCoordinate2D {
+        return .init(latitude: 25.7602, longitude: -80.1959)
+    }
+}
+
+extension MKCoordinateRegion {
+    static var userRegion: MKCoordinateRegion {
+        return .init(center: .userLocation,
+                        latitudinalMeters: 10000,
+                        longitudinalMeters: 10000)
+//        possibly change this decreasing for a closer view
+    }
+}
+
+
+
 
 #Preview {
     ContentView()
---
+++ README.md
@@ -0,0 +1,20 @@
+# My Maps
+
+Overview
+Project Type: Maps Aplication
+Features: Maps application where user can search for near by places and plan a route to get there. 
+
+## Tech Stack
+
+- Swift
+- SwiftUI
+
+
+## Application
+
+|   Description    |   Image    |
+|:------------:|:----------------:|
+|Map| ![](images/map-img.png)|
+|Search| ![](images/search-img.png)|
+|View Place| ![](images/view-place.png)|
+|Plan Route| ![](images/route-img.png)|
